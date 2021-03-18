4395f36cb43de968bd72cf6c84c18d80710bf98f
Arturo Velarde <arturovr82@hotmail.com>
2021-03-17 23:42:01 -0300

    TapeEquilibrium Example

--- TapeEquilibrium/Program.cs
+++ TapeEquilibrium/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TapeEquilibrium
 {
@@ -7,7 +8,8 @@ namespace TapeEquilibrium
         static void Main(string[] args)
         {
             var input = new int[] {3,1,2,4,3};
-            var result = solution(input);
+            // var result = solution(input);
+            var result = solution2(input);
             Console.WriteLine(result);
         }
 
@@ -35,5 +37,25 @@ namespace TapeEquilibrium
             return sum;
         }
 
+        static int solution2(int[] A){
+            int min = Int32.MaxValue;
+            int leftSum = 0;
+            int rightSum = 0;
+            int delimiter = 1;
+            while (delimiter <= A.Length - 1)
+            {
+                leftSum += A[delimiter - 1];
+                if(rightSum == 0)
+                    rightSum = Sum(A, delimiter, A.Length); 
+                else
+                    rightSum -= A[delimiter - 1]; 
+
+                var diff = Math.Abs(leftSum - rightSum);
+                 if(diff < min)
+                    min = diff;
+                delimiter++;
+            }
+            return min;
+        }
     }
 }
